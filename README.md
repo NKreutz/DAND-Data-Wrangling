a123d090420a76fdb9178b2b3811437af432d7e2
Narissa Kreutz <narissa.kreutz@gmail.com>
2017-10-02 15:12:23 -0600

    add sql_queries.py

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ Project Overview:
 
 To complete the project, I was required to choose an area from https://www.openstreetmap.org and use data munging techniques, such as assessing the quality of the data for validity, accuracy, completeness, consistency and uniformity, to clean the OpenStreetMap data. The next step was to store, query, and aggregate data using SQL.
 
-I chose to complete this project on the [Edmonton, Alberta, Canada](https://mapzen.com/data/metro-extracts/metro/edmonton_canada/) area. The challenges I encountered during the wrangling phase and results of the SQL queries I ran are documented in the review.pdf file.
+I chose to complete this project on the [Edmonton, Alberta, Canada](https://mapzen.com/data/metro-extracts/metro/edmonton_canada/) area. The challenges I encountered during the wrangling phase and results of the SQL queries I ran are documented in the Wrangling_data_in_SQL.pdf file.
 
 ### Dependencies
 
@@ -14,14 +14,15 @@ This project is written in Python 3 and uses a SQLite Database
 ### Installation/Usage (Mac)
 
 1. Clone the [Data_Wrangling](https://github.com/NKreutz/DAND-Data-Wrangling) repository
-2. `cd` into the data_wrangling_with_SQL folder
+2. `cd` into the Data_wrangling_with_SQL folder
 3. Create the conda environment:
     * `conda env create -f dand-env-mac.yaml`
 4. install the cerberus module:
     * `conda install cerberus`
 5. Activate the Conda environment
     * `source activate DAND`
-4. Create the SQLite Database by running these commands in your terminal:
+6. Unzip "csvs.zip" and move the files into your working directory
+7. Create the SQLite Database by running these commands in your terminal:
     * `sqlite3 edmonton_openstreetmaps.db`
     * sqlite> `.mode csv`
     * sqlite> `.import nodes_tags.csv nodes_tags`
@@ -30,7 +31,7 @@ This project is written in Python 3 and uses a SQLite Database
     * sqlite> `.import ways_tags.csv ways_tags`
     * sqlite> `.import ways.csv ways`
     * sqlite> `.quit`
-5. Run `python sql_queries.py` to query the Database
+8. Run `python sql_queries.py` to query the Database
 
 
 ### Credits
--- sql_queries.py
+++ sql_queries.py
@@ -5,13 +5,89 @@ import pprint
 
 connection = sqlite3.connect("edmonton_openstreetmaps.db")
 cursor = connection.cursor()
-query = "select value from nodes_tags where key = 'street'"
-cursor.execute(query)
-rows = cursor.fetchall() # or use cursor.fetchone() for one at a a time
-pprint.pprint(rows) # or loop over the rows
 
-query2 = "select value from nodes_tags where key = 'city'"
-cursor.execute(query2)
-rows = cursor.fetchall() # or use cursor.fetchone() for one at a a time
-pprint.pprint(rows) # or loop over the rows
+# Create Views to speed up searches
+view1 = '''create view ways_nodes_tags_view
+as select * from nodes_tags
+union all
+select * from ways_tags
+'''
+
+# Count how unique users there are (copied from SQL project example)
+query1 = ''' select count(distinct(e.uid))
+from (select uid from nodes
+union all
+select uid from ways) as e
+'''
+
+# Count number of Nodes
+query2 = ''' select count(*)
+from nodes
+'''
+
+# Count number of Ways
+query3 = ''' select count(*)
+from ways
+'''
+
+# Count how many regions/cities are represented in the dataset
+query4 = ''' select count(*)
+from (select value, count(*) as num
+from ways_nodes_tags_view
+where key = 'city'
+group by value)
+'''
+
+# Count how many entries there are for each community, and return the top 10
+query5 = '''select value, count(*) as num
+from ways_nodes_tags_view
+where key = 'city'
+group by value
+order by num desc
+limit 10
+'''
+
+# Count how many entries there are for each community, and return those
+# with only 1 entry
+query6 = '''select value, count(*) as num
+from ways_nodes_tags_view
+where key = 'city'
+group by value
+having num = 1
+'''
+
+#Find out what amenities are listed, return top 10
+query7 = '''select value, count(*) as num
+from ways_nodes_tags_view
+where key = 'amenity'
+group by value
+order by num desc
+limit 10
+'''
+
+#Find the names of the most common restaurants
+query8 = ''' select value, count(*) as num
+from (select b.value as value from ways_nodes_tags_view as a, ways_nodes_tags_view as b
+where a.id = b.id
+and (a.value = "fast_food" or a.value = "restaurant")
+and b.key = "name"
+group by a.id)
+group by value
+having num > 1
+order by num desc
+'''
+
+all_views = [view1]
+all_queries = [query1, query2, query3, query4, query5, query6, query7, query8]
+
+cursor.execute(view1)
+count = 0
+for query in all_queries:
+    count += 1
+    cursor.execute(query)
+    rows = cursor.fetchall()
+    print "Query #" + str(count)
+    pprint.pprint(rows)
+
+
 connection.close
